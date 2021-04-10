743b9366d522c11773f4734070925ee24c2b0f13
TheDusKnight <860956020@qq.com>
2021-04-10 02:43:00 -0700

    finish basic card

--- frontend/mammals-app/src/components/Ecoregion.js
+++ frontend/mammals-app/src/components/Ecoregion.js
@@ -7,7 +7,10 @@ const { Panel } = Collapse;
 const { Meta } = Card;
 
 function Describe(props) {
-    // console.log(props.faunaMammals)
+    var coordinates = [];
+    if (props.ecoInfo.ecoregion__coordinates) {
+        coordinates = JSON.parse(props.ecoInfo.ecoregion__coordinates.replace(/'/g, '"'));
+    }
     return (
         <div>
             <Row style={{ marginTop: 20 }} justify="center">
@@ -26,14 +29,36 @@ function Describe(props) {
                         <Descriptions.Item label="Biome">{props.ecoInfo.ecoregion__Biome}</Descriptions.Item>
                         <Descriptions.Item label="Country">{props.ecoInfo.ecoregion__Country}</Descriptions.Item>
                         <Descriptions.Item label="Conservation status">{props.ecoInfo.ecoregion__Conservation_status}</Descriptions.Item>
-                        <Descriptions.Item label="Coordinates">{props.ecoInfo.ecoregion__coordinates}</Descriptions.Item>
+                        <Descriptions.Item label="Coordinates">{coordinates[0]}<br />{coordinates[1]}</Descriptions.Item>
                         <Descriptions.Item label="Area">{props.ecoInfo.ecoregion__Area}</Descriptions.Item>
-                        <Descriptions.Item label="url" span={3}><a target="_blank" href={props.ecoInfo.ecoregion__url} rel="noreferrer">{props.ecoInfo.ecoregion__url}</a></Descriptions.Item>
-                        <Descriptions.Item label="uri" span={3}>{props.ecoInfo.uri}</Descriptions.Item>
+                        <Descriptions.Item label="URL" span={3}><a target="_blank" href={props.ecoInfo.ecoregion__url} rel="noreferrer">
+                            {props.ecoInfo.ecoregion__url}</a></Descriptions.Item>
+                        <Descriptions.Item label="URI" span={3}>{props.ecoInfo.uri}</Descriptions.Item>
                     </Descriptions>
                 </Col>
             </Row>
         </div>
+    )
+}
+
+function MyCard(props) {
+    return (
+        <Col span={6}>
+            <Card
+                size="small"
+                hoverable
+                bordered={false}
+                cover={<Image
+                    className="img"
+                    src={props.item.non_mammal__icon}
+                    height={180}
+                    width={180}
+                    fallback={ErrorImg}
+                />}
+            >
+                <Meta title={props.item.non_mammal__name} description={props.item.non_mammal__taxonName} />
+            </Card>
+        </Col>
     )
 }
 
@@ -70,7 +95,8 @@ function Accordion(props) {
             }
             {props.faunaMammalSubs.length > 0 &&
                 <Panel header="Fauna mammals subspecies" key="2">
-                    <p>{text}</p>
+                    {/* <p>{text}</p> */}
+
                 </Panel>
             }
             {props.faunaNonMammals.length > 0 &&
@@ -79,23 +105,10 @@ function Accordion(props) {
                     <div className="site-card-wrapper">
                         <Row gutter={[16, 16]}>
                             {props.faunaNonMammals.map((item) => (
-                                <Col span={8}>
-                                    <Card 
-                                        hoverable 
-                                        bordered={false}
-                                        cover={<Image
-                                            className="img"
-                                            src={item.non_mammal__icon}
-                                            height={180} 
-                                            width={180}
-                                            fallback={ErrorImg}
-                                        />}
-                                    >
-                                        <Meta title={item.non_mammal__name} description={item.non_mammal__taxonName} />
-                                    </Card>
-                                </Col>
+                                <MyCard
+                                    item={item}
+                                />
                             ))}
-
                         </Row>
                     </div>
                 </Panel>
@@ -103,6 +116,15 @@ function Accordion(props) {
             {props.floras.length > 0 &&
                 <Panel header="Floras" key="4">
                     {/* <p>{text}</p> */}
+                    <div className="site-card-wrapper">
+                        <Row gutter={[16, 16]}>
+                            {props.floras.map((item) => (
+                                <MyCard
+                                    item={item}
+                                />
+                            ))}
+                        </Row>
+                    </div>
                 </Panel>
             }
 
@@ -148,9 +170,11 @@ export class Ecoregion extends Component {
         return (
             <div>
                 <Divider orientation="center" style={{ fontSize: 30 }}> Basic Information </Divider>
-                <Describe
-                    ecoInfo={this.state.ecoInfo}
-                />
+                {this.state.ecoInfo &&
+                    <Describe
+                        ecoInfo={this.state.ecoInfo}
+                    />
+                }
                 <Divider orientation="center" style={{ fontSize: 30 }}> Fauna & Flora </Divider>
                 <Accordion
                     faunaMammals={this.state.faunaMammals}
